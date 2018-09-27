c4240e5ef78e86e717c8297ce02b232ba5253354
SadP4nda <ianm0127@hotmail.com>
2018-09-27 14:30:38 -0400

    Added a linear prediction model that predicts the closing price through the open,high, and low price as well as the volume and market cap

--- test.py
+++
@@ -1,6 +0,0 @@
-import time
-import requests
-from bs4 import BeautifulSoup
-x = time.time()
-BeautifulSoup(requests.get("https://coinmarketcap.com/currencies/bitcoin/historical-data/").text, 'lxml')
-print(time.time()-x)
